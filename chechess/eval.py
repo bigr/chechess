b7f6bc53c734a59fc8a12aa707a5d3771fff7332
klinger <ja@bigr.cz>
2021-09-12 11:22:11 +0200

    MixEvaluator implementation

--- chechess/eval.py
+++ chechess/eval.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Mapping, Sequence
+from typing import Mapping, Optional, Sequence
 
 import chess
 from chess import Board
@@ -31,4 +31,16 @@ class PieceSquareEvaluator(Evaluator):
             score_table[square]
             for piece, score_table in self.piece_square_scores.items()
             for square in board.pieces(piece.piece_type, piece.color)
+        )
+
+
+class MixEvaluator(Evaluator):
+    def __init__(self, evaluators: Sequence[Evaluator], weights: Optional[Sequence[float]]):
+        self.evaluators = evaluators
+        self.weights = weights if weights else [1.0] * len(self.evaluators)
+
+    def eval(self, board: Board) -> float:
+        return sum(
+            weight * evaluator.eval(board)
+            for evaluator, weight in zip(self.evaluators, self.weights)
         )
